996fc2cdbedfa5a9f100916f3de598021855e7d7
Eliezer Bittencourt <likobittencourt@gmail.com>
2021-09-30 18:31:55 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-$('#form-contato').submit(function () {
-    var nome = $('#nome');
-    var sobrenome = $('#sobrenome');
-    var data = $('#data');
-    var cpf = $('#cpf');
-    var celular = $('#celular');
-    var email = $('#email');
-    var cep = $('#cep');
-    var endereco = $('#endereco');
-    var numero = $('#numero');
-    var cidade = $('#cidade');
-    var uf = $('#uf');
-    var descricao = $('#descricao');
+//valida se usuario preencheu informações
+$('#formulario').submit(function () {
+    var nome = $(this).val();
+    var nascimento = $(this).val();
+    var cpf = $(this).val();
+    var telefone = $(this).val();
+    var email = $(this).val();
+    var cep = $(this).val();
+    var rua = $(this).val();
+    var numero = $(this).val();
+    var bairro = $(this).val();
+    var cidade = $(this).val();
+    var uf = $(this).val();
+    var descricao = $(this).val();
     var erro = $('.alert');
     var campo = $('#campo-erro');
 
@@ -27,21 +28,12 @@ $('#form-contato').submit(function () {
         return false;
     }
 
-    // valida o campo sobrenome
-    if (sobrenome.val() == '') {
-        erro.removeClass('d-none');
-        campo.html('sobrenome'); // nome do campo que não foi preenchido!
-        sobrenome.focus();
-        sobrenome.addClass('is-invalid');
-        return false;
-    }
-
     // valida o campo nascimento
-    if (data.val() == '') {
+    if (nascimento.val() == '') {
         erro.removeClass('d-none');
-        campo.html('data'); // nome do campo que não foi preenchido!
-        data.focus();
-        data.addClass('is-invalid');
+        campo.html('nascimento'); // nome do campo que não foi preenchido!
+        nascimento.focus();
+        nascimento.addClass('is-invalid');
         return false;
     }
 
@@ -54,12 +46,12 @@ $('#form-contato').submit(function () {
         return false;
     }
 
-    // valida o campo celular
-    if (celular.val() == '') {
+    // valida o campo telefone
+    if (telefone.val() == '') {
         erro.removeClass('d-none');
-        campo.html('celular'); // nome do campo que não foi preenchido!
-        celular.focus();
-        celular.addClass('is-invalid');
+        campo.html('telefone'); // nome do campo que não foi preenchido!
+        telefone.focus();
+        telefone.addClass('is-invalid');
         return false;
     }
 
@@ -81,12 +73,12 @@ $('#form-contato').submit(function () {
         return false;
     }
 
-    // valida o campo endereco
-    if (endereco.val() == '') {
+    // valida o campo rua
+    if (rua.val() == '') {
         erro.removeClass('d-none');
-        campo.html('endereco'); // nome do campo que não foi preenchido!
-        endereco.focus();
-        endereco.addClass('is-invalid');
+        campo.html('rua'); // nome do campo que não foi preenchido!
+        rua.focus();
+        rua.addClass('is-invalid');
         return false;
     }
 
@@ -134,10 +126,53 @@ $('#form-contato').submit(function () {
         descricao.addClass('is-invalid');
         return false;
     }
-    
-    
     // se chegar aqui pode enviar os dados!
     return true;
 });
- 
 
+//Preenchimento dos dados de endereço utilizado ViaCep
+$("#cep").focusout(function(){
+    //Início do Comando AJAX
+    $.ajax({
+        //O campo URL diz o caminho de onde virá os dados
+        url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
+        dataType: 'json',
+        //SUCESS é referente a função que será executada caso
+        //ele consiga ler a fonte de dados com sucesso.
+        //O parâmetro dentro da função se refere ao nome da variável
+        //que vai dar para ler esse objeto.
+        success: function(resposta){
+            //Agora os valores irão preencher
+            //automaticamente nos campos acima.
+            $("#rua").val(resposta.logradouro);
+            $("#bairro").val(resposta.bairro);
+            $("#cidade").val(resposta.localidade);
+            $("#uf").val(resposta.uf);
+            //O Número será focado automaticamente
+            //melhorando a experiência do usuário
+            $("#numero").focus();
+        }
+    });
+});
+
+//mascara para o telefone e cpf
+var behavior = function (val) {
+    return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+},
+options = {
+    onKeyPress: function (val, e, field, options) {
+        field.mask(behavior.apply({}, arguments), options);
+    }
+};
+
+$('.telefone').mask(behavior, options);
+
+$('.cpf').mask('000.000.000-00', {reverse: true});
+
+//retorna valores digitados no console
+$("#formulario").focusout(function(){
+    var dados = JSON.stringify( $('#formulario').serializeArray() ); 
+
+    console.log("Dados corretos:" + dados);
+   
+});
